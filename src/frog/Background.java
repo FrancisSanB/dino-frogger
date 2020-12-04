f88c9f831e2b72d1463c17da66741ca7df70e206
Francis Fernandez <fosb.promo@gmail.com>
2020-12-04 08:40:53 -0800

    initial commit

---
+++ src/frog/Background.java
@@ -0,0 +1,124 @@
+package frog;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class Background{
+	// attributes of a frog
+	private int x, y; // Position of frog
+	private int width; // the size of frog
+	private int height;
+	private double scaleX, scaleY;
+	
+	private Image img; // image of the frog
+	
+	/* if filename is provided */
+	public Background(String fileName, int x, int y, double scaleX, double scaleY) {
+		// assignment statements for attributes
+		this.x = x;
+		this.y = y;
+		this.scaleX = scaleX;
+		this.scaleY = scaleY;
+		width = 50;
+		height = 50;
+		img = getImage(fileName);
+		init(x, y);
+
+	}
+
+	/*public void reset() {
+		x = 200;	//reset position
+		y = 500;
+		img = up;	//reset img
+	}*/
+	
+	public void move() {
+		tx.setToTranslation(x, y);
+	}
+
+	/*//move the main dino
+	public void hop(KeyEvent e) {
+		if (e.getKeyCode() == 83 || e.getKeyCode() == 40) {
+			y += 50;
+		}
+		if (e.getKeyCode() == 87 || e.getKeyCode() == 38) {
+			y -= 50;
+		}
+		if (e.getKeyCode() == 68 || e.getKeyCode() == 39) {
+			x += 50;
+		}
+		if (e.getKeyCode() == 65 || e.getKeyCode() == 37) {
+			x -= 50;
+		}
+		tx.setToTranslation(x, y);
+		
+	}*/
+	
+	
+	private AffineTransform tx = AffineTransform.getTranslateInstance(x, y);
+
+	// draw the affine transform
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		move(); //ask frog to update its location variables
+		tx.scale(scaleX, scaleY);
+		g2.drawImage(img, tx, null);
+		
+	}
+
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+	}
+
+	// converts image to make it drawable in paint
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Background.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+
+	// setters and getters
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+		tx.setToTranslation(x, y);
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+		tx.setToTranslation(x, y);
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	/* Helper function for collision detection later */
+	public Rectangle getRect() {
+		Rectangle temp = new Rectangle(x,y,width,height);
+		return temp;
+	}
+
+}
---
+++ src/frog/Driver.java
@@ -0,0 +1,158 @@
+package frog;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import java.awt.image.*;
+import java.awt.geom.AffineTransform;
+
+public class Driver extends JPanel implements ActionListener, KeyListener, MouseListener {
+
+	Player dino;
+	Background grass;
+	Background ketchup;
+	
+	//Froggy frogs[] = new Froggy[10];
+
+	Font big = new Font("Courier New", 1, 50);
+	Font font2 = new Font("Courier New", 1, 30);
+	Font biggest = new Font("Courier New", 1, 80);
+	
+	int width = 560;
+	int height = 800;
+	
+	// ****************************paint
+	// method******************************************
+	public void paint(Graphics g) {
+		super.paintComponent(g);
+		
+
+		ketchup.paint(g);
+		grass.paint(g);
+		dino.paint(g);
+		
+		/*for(Froggy temp: frogs) {
+			temp.paint(g);
+		}*/
+		
+		if (dino.getX() < 0) {
+			dino.setX(250);
+			dino.setY(700);
+		}
+		
+		if (dino.getX() >= width - 10) {
+			dino.setX(250);
+			dino.setY(700);
+		}
+
+		System.out.println(dino.getX() + "," + dino.getY());
+	}
+
+	public void update() {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		update();
+		repaint();
+	}
+
+	public static void main(String[] arg) {
+		Driver d = new Driver();
+	}
+
+	public Driver() {
+		JFrame f = new JFrame();
+		f.setTitle("Frogger");
+		f.setSize(width, height);
+		f.setResizable(false);
+		f.addKeyListener(this);
+		
+		dino = new Player();
+		ketchup = new Background("ketchupriver.png", 0, 0, 3, 3);
+		grass = new Background("grass.png", 0, 300, 1.25, 1.25);
+		
+		/*for (int i = 0; i < frogs.length; i ++) {
+			frogs[i] = new Froggy();
+			frogs[i].setX(i*50);
+		}*/
+
+	
+		f.addMouseListener(this);
+		f.add(this);
+		t = new Timer(17, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+	Timer t;
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		dino.hop(e);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		/*
+		 * turn off velocity for Frog if you don't want it moving when you have stopped
+		 * pressing the keys
+		 */
+
+		// do the same thing for the other keys
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
